4820a2a27d26c94acd1d4c3ee441385eb0a32946
sheppel <sheppelmann@ucdavis.edu>
2021-06-05 14:20:30 -0400

    typo

--- FemtoProtonAnalysis/scripts/hadd_profiles.sh
+++ FemtoProtonAnalysis/scripts/hadd_profiles.sh
@@ -3,11 +3,11 @@
 config_file=../../femto.config
 source $config_file
 currentDir=`pwd`
-$profile_dir=$PROFILE_DIR_OUT
-$profile_dir_local=$ANA_DIR/rootfiles/profiles
+profile_dir=$PROFILE_DIR_OUT
+profile_dir_local=$ANA_DIR/rootfiles/profiles
 
 cp AddAll.sh $profile_dir
-cp MadAdder.y $profile_dir
+cp MadAdder.py $profile_dir
 
 cd $profile_dir
 ./AddAll.sh
--- FemtoProtonAnalysis/scripts/run_corr_and_plot.sh
+++ FemtoProtonAnalysis/scripts/run_corr_and_plot.sh
@@ -13,15 +13,15 @@ $pileup_corr=$PU_NORM_FILE
 $pileup_corr_high=$PU_HIGH_FILE
 $pileup_corr_low=$PU_LOW_FILE
 
-root -l -q -b ../macros/RunPileUpCorrections.C\(\"$profile_dir_local\", \"$cumulant_dir_local\", \"$pileup_corr\", \"$pileup_corr_low\", \"$pileup_corr_high\"\)
-wait
+#root -l -q -b ../macros/RunPileUpCorrections.C\(\"$profile_dir_local\", \"$cumulant_dir_local\", \"$pileup_corr\", \"$pileup_corr_low\", \"$pileup_corr_high\"\)
+#wait
 
-root -l -q -b ../macros/CalcSystematic.C\(\"$cumulant_dir_local\"\)
-wait
+#root -l -q -b ../macros/CalcSystematic.C\(\"$cumulant_dir_local\"\)
+#wait
 
-root -l -q -b ../macros/MakeTopFive.C
-wait
+#root -l -q -b ../macros/MakeTopFive.C
+#wait
 
-root -l -q -b ../macros/DrawCumulant.C
+#root -l -q -b ../macros/DrawCumulant.C
 
 
